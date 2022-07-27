bb94b99c4c00955d1f83f29f60096501fa52d0b2
careertakada <careeruser31@gmail.com>
2022-07-27 13:17:44 +0900

    momotarto

--- welcomeapp/models.py
+++ welcomeapp/models.py
@@ -2,13 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Visitors(models.Model):
-    person_name = models.CharField(max_length=20)
-    company = models.CharField(max_length=30)
-    acompany = models.CharField(max_length=30, null=True) # 同伴者名
+    person_name = models.CharField(max_length=20, null=True)
+    company = models.CharField(max_length=30, null=True)
+    accompany = models.CharField(max_length=30, null=True) # 同伴者名
     person = models.IntegerField(null=True) # 同伴人数
     text1 = models.CharField(max_length=30, null=True)
+    tantousya = models.CharField(max_length=30, null=True)# 担当者名
+    dates = models.DateTimeField(null=True)# 日付
+    affair1 = models.CharField(max_length=30,)#用件
+    affair2 = models.CharField(max_length=30, null=True)#その他の用件
+
+
+    #times=models.DateTimeField(auto_now_add=True, null=True)
+
     # entry_time = models.TimeField()
     # created = models.DateTimeField(auto_now_add=True, help_text='作成日')
     # modified = models.DateTimeField(auto_now=True, help_text='更新日')
 
-    visit_time=models.DateTimeField(auto_now_add=True, null=True)
+    #visit_
+    times=models.DateTimeField(auto_now_add=True, null=True)
--- welcomeapp/views.py
+++ welcomeapp/views.py
@@ -1,3 +1,4 @@
+from os import times
 from django.shortcuts import render, redirect
 from sklearn.metrics import precision_score
 from .models import Visitors
@@ -11,23 +12,36 @@ def output(request):
         name = request.POST.get('input_name')
         name1 = request.POST.get('company')
         name2 = request.POST.get('person')
-        name3 = request.POST.get('position')
-        name4 = request.POST.get('acompany')
+        name3 = request.POST.get('tantousya')
+        name4 = request.POST.get('accompany')
         name5 = request.POST.get('text1')
-        Visitors.objects.create(person_name=name,company=name1,acompany=name4,text1=name5,person=name2)
+        name6 = request.POST.get('dates')
+        name7 = request.POST.get('time')
+        name8 = request.POST.get('affair1')
+        name9 = request.POST.get('affair2')
+        Visitors.objects.create(person_name=name,company=name1,accompany=name4,text1=name5,person=name2,dates=name6,\
+            times=name7,tantousya=name3,affair1=name8,affair2=name9)
         context = {
             'output_name': name,
             'company_name': name1,
             'person': name2,
-            'position': name3,
-            'acompany': name4,
+            'tantousya': name3,
+            'accompany': name4,
             'text1': name5,
+            'dates': name6,
+            'time': name7,
+            'affair1':name8,
+            'affair2':name9,
 
             }
         print(name)
         print(context)
+
+
     return render(request, 'output.html', context)
 
+    
+     
 def kanri(request):
    visitors_list=Visitors.objects.all()
    context={'visitors':visitors_list}
@@ -41,16 +55,22 @@ def edit(request, pk):
         ninzu=request.POST.get('edit3')
         entry_time=request.POST.get('edit4')
         texts=request.POST.get('edit25')
-        acompany=request.POST.get('edit5')
+        accompany=request.POST.get('edit5')
+        texts1=request.POST.get('edit26')
+
+        print(name)
+        print(company_name)
 
         visitor_object.person_name=name
         visitor_object.company=company_name
         visitor_object.person=ninzu
         visitor_object.visit_time=entry_time
-        visitor_object.text1=texts
-        visitor_object.acompany=acompany
+        visitor_object.affair1=texts
+        visitor_object.accompany=accompany
+        visitor_object.affair2=texts1
         visitor_object.save()
         return redirect('kanri')
+
     else:
         # 下にcontextを後で追加
         context={'visitor':visitor_object}
